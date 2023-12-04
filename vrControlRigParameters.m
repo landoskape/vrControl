c1d3b6286941d8e0b00b58524a30838c45024241
OrangeRigs <ennyvanbeest@gmail.com>
2023-12-04 16:45:33 +0000

    Add zaanland details

--- vrControlRigParameters.m
+++ vrControlRigParameters.m
@@ -173,6 +173,61 @@ switch upper(hostname)
         rigInfo.sendTTL = 0; % ttl not necessary here for timeline sync
         rigInfo.TTLchannel = 'Port0/Line0'; %might be flipped.
         
+    
+    case 'ZAANLAND'
+        % Local computer info (basic)
+        rigInfo.computerName = 'ZAANLAND';
+        rigInfo.localFigurePosition = [4400 -85];
+        rigInfo.screenNumber = 2; % Checked
+        rigInfo.screenDist = 10; 
+        rigInfo.dialogueXYPosition = [4400 -85]; %to check (parameters for GUI)
+        
+        rigInfo.NIdevID = 'Dev2';
+        rigInfo.NIsessRate = 5000;
+        rigInfo.NIRotEnc = 'ctr1';
+        rigInfo.NILicEnc = ''; 
+        rigInfo.NIRewVal = 'ao0';
+        rigInfo.photodiodePos  = 'right';
+        rigInfo.photodiodeSize = [100 75]; % [650 190] %% Enny: CHECK   %[250 75];
+        rigInfo.rotEncPos = 'left'; 
+        rigInfo.rotEncSign = -1; 
+        rigInfo.wheelToVR = 4000;
+        rigInfo.wheelRadius = 10; 
+        rigInfo.lickEncoderAvailable = false; 
+
+        rigInfo.waterVolumeSTOP = 0.0;
+        rigInfo.waterVolumeBASE = 8;
+        rigInfo.waterVolumePASS = 8;
+        rigInfo.waterVolumeACTV = 8;
+        rigInfo.PrefRefreshRate = 30;
+
+        % Saving directories: remote
+        rigInfo.dirSave = 'C:\Users\Experiment\Documents\animalData';
+        rigInfo.dirScreenCalib = 'C:\Users\Experiment\Documents\GitHub\vrControlGratings';
+        rigInfo.filenameScreenCalib = 'halfcyclindercalib_22102021.mat';
+        
+        rigInfo.WaterCalibrationFile = 'Zeeland_water_calibs'; %Change!!
+
+        % Timeline
+        rigInfo.connectIPs{1} = '128.40.198.104';
+        rigInfo.connectPCs{1} = 'ZAANLAND';
+        rigInfo.connectPortnr{1} = 1001;
+
+        % EYE CAMERA
+        rigInfo.connectIPs{2} = '128.40.198.227';
+        rigInfo.connectPCs{2} = 'ZOOLAND';
+        rigInfo.connectPortnr{2} = 1001;
+
+        % EYE CAMERA
+        rigInfo.connectIPs{3} = '128.40.198.227';
+        rigInfo.connectPCs{3} = 'ZOOLAND';
+        rigInfo.connectPortnr{3} = 1002;
+
+        rigInfo.numConnect = length(rigInfo.connectIPs);
+        rigInfo.sendTTL = 0; % ttl not necessary here for timeline sync
+        rigInfo.TTLchannel = 'Port0/Line0'; %might be flipped.
+        
+
     case 'DESKTOP-C0GU3US'
         rigInfo.localFigurePosition = [680 500];
         rigInfo.dirSave = 'C:\Users\andrew\Documents\GitHub\vrControl\settingsFolder';
